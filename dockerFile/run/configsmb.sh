1f1361eaeb18ae6777c3274a022762782a12dfb3
Informatico Del Averno <informaticodelaverno@gmail.com>
2020-03-19 15:49:29 -0700

    Modificacion de configuracion para eliminar volumenes compartidos a priori innecesarios.

--- dockerFile/run/configsmb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "[shared]" >> /etc/samba/smb.conf
-echo "  comment = Samba" >> /etc/samba/smb.conf
-echo "  path = /shared" >> /etc/samba/smb.conf
-echo "  read only = no" >> /etc/samba/smb.conf
-echo "  browsable = yes" >> /etc/samba/smb.conf
-echo "  guest ok = yes" >> /etc/samba/smb.conf
-echo "  guest only = yes" >> /etc/samba/smb.conf
